3094fe7293c09737f3016266197592dd3916996b
Emerson Lourenzi <emersonl.lourenzi@gmail.com>
2019-10-07 15:56:11 -0300

    Going up dropbox challenge project for github

--- src/main/java/com/challange/ChallengeApplication.java
+++ src/main/java/com/challange/ChallengeApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableMongoRepositories("com.challenge.impl.user.repository")
 public class ChallengeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChallengeApplication.class, args);
